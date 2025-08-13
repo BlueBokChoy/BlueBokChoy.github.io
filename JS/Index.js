06148e1d4c6da2050e283f90ee777888d911d7ba
Roy Chen <royc0133@gmail.com>
2025-08-13 12:21:49 -0400

    Changed menu opening style

--- JS/Index.js
+++ JS/Index.js
@@ -7,10 +7,8 @@
 function openmenu() {
 	if (document.getElementById("menuopt").offsetHeight == "0") {
 		document.getElementById("menuopt").style.height = "20vw"
-		document.getElementById("menuopt").style.width = "15vw";
 	}
 	else {
 		document.getElementById("menuopt").style.height = "0";
-		document.getElementById("menuopt").style.width = "0";
 	}
 }
